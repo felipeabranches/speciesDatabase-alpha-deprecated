69b2d5349b1d3a9d22428052f74ca9968493641c
TNMedeiros <45217035+TNMedeiros@users.noreply.github.com>
2018-12-06 20:19:56 -0200

    Sql Statement done
    Please check and commit the file.

---
+++ includes/db/wpt_country_install.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `wpt_country` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) NOT NULL,
+  `nicename` varchar(80) NOT NULL,
+  `iso` char(2) NOT NULL,
+  `iso3` char(3) DEFAULT NULL,
+  `numcode` smallint(6) DEFAULT NULL,
+  `phonecode` int(5) NOT NULL,
+  `description` varchar(5120) COLLATE utf8_bin NOT NULL,
+  `note` varchar(255) COLLATE utf8_bin NOT NULL,
+  `image` varchar(255) COLLATE utf8_bin NOT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
