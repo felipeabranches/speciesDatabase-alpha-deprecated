f4392246b8e75a2ac83f5e21b34e1d918a0ed3b1
Felipe Abranches <felipeabranches@users.noreply.github.com>
2018-12-13 09:15:23 -0200

    Update sp_species.php

--- admin/sp_species.php
+++ admin/sp_species.php
@@ -137,7 +137,5 @@ $order_by = $_GET['order_by'];
     </div>
 </div>
 <?php include_once '../modules/footer.php'; ?>
-<!-- Fontawesome -->
-<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 </body>
 </html>
