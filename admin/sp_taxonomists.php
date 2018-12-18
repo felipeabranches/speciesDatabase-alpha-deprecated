be4c7541a5dcd0489b46361315131d15afea5389
SinvalVJunior <40250767+SinvalVJunior@users.noreply.github.com>
2018-12-18 18:34:08 -0200

    File modified to OOP
    File with the necessary changes to OOP ,
    Creates the $filter_by variable and calls the new getTaxonomists function
    Changes to access the variables in the Taxonomists object

---
+++ admin/sp_taxonomists.php
@@ -0,0 +1,104 @@
+<?php
+include_once '../init.php';
+$page_title = 'Taxonomists';
+$page_count = 10;
+$id = $_GET['id'];
+$order_by = $_GET['order_by'];
+include_once '../libraries/species/taxonomists.php';
+$taxonomists = new Taxonomists;
+$filter_by = 'no filter';
+$result = mysqli_query($mysqli, $taxonomists->getTaxonomists($id, $order_by,$filter_by));
+?>
+<!doctype html>
+<html lang="pt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="<?php echo $author; ?>">
+	<title><?php echo $page_title; ?> - <?php echo $site_name; ?></title>
+    <?php include_once '../modules/head.php'; ?>
+</head>
+
+<body class="bg-light">
+<?php include_once 'modules/menu.php'; ?>
+<div class="container-fluid" role="main">
+    <div class="toolbar sticky-top row my-2 p-2">
+        <div class="col-12 col-md-10">
+            <h4><?php echo $page_title; ?></h4>
+        </div>
+        <div class="col-12 col-md-2">
+            <a href="sp_taxonomist.php?id=0" class="btn btn-primary float-right" role="button">New</a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="my-3 p-3 bg-white rounded box-shadow">
+                <?php
+                if (!$result->num_rows)
+                {
+                    echo '<span>No entries</span>';
+                }
+                else
+                {
+                ?>
+                <!-- Table -->
+                <table class="table table-striped table-hover table-sm">
+                    <tr width="100%">
+                        <th width="5%"><a href="sp_taxonomists.php?order_by=id">ID</a></th>
+                        <th width="90%"><a href="sp_taxonomists.php?order_by=name">Name</a></th>
+                        <th width="5%" colspan="2"><a href="sp_taxonomists.php?order_by=published">State</a></th>
+                    </tr>
+                <?php
+                    // Fetch one and one row
+                    while ($row = mysqli_fetch_object ($result))
+                    {
+                        if ($row->published)
+                        {
+                            $published='<i class="fas fa-toggle-on"></i>';
+                        }
+                        else
+                        {
+                            $published='<i class="fas fa-toggle-off"></i>';
+                        }
+                        echo '<tr>';
+                        echo '<td>'.$row->id.'</td>';
+                        echo '<td><a href="sp_taxonomist.php?id='.$row->id.'">'.$row->name.'</a></td>';
+                        echo '<td>'.$published.'</td>';
+                        echo '<td><a data-toggle="modal" data-target="#modal-'.$row->id.'"><i class="fas fa-trash-alt"></i></a></td>';
+                        echo '</tr>'."\n";
+                        echo '<!-- Modal -->
+                            <div class="modal" id="modal-'.$row->id.'" tabindex="-1" role="dialog">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title">Delete</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Are you sure you want to delete <strong>'.$row->name.'</strong> (ID: '.$row->id.')?</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <a href="modules/sp_taxonomists_delete.php?id='.$row->id.'" class="btn btn-danger">Delete</a>
+                                            <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>';
+                    }
+                    // Free result set
+                    mysqli_free_result ($result);
+                ?>
+                </table>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include_once '../modules/footer.php'; ?>
+</body>
+</html>
